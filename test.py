5e5c3cadf7896ae351c3dadd0a146d943481ba77
Maksim Muravev <halloumee@proton.me>
2023-05-07 09:10:15 +0300

    Fix decode_percent

--- test.py
+++ test.py
@@ -3,15 +3,9 @@ import logging
 import logging.handlers
 import random
 
-from urllib.parse import unquote_plus
-
 vector_host = 'vector-xl4avw.bunnyenv.com'
 vector_url = '/'
 handler = logging.handlers.HTTPHandler(vector_host, vector_url, method='POST', secure=True)
-
-# Set the Content-Type header to text/plain
-handler.setFormatter(logging.Formatter('%(message)s'))
-handler.addFilter(lambda record: not record.args)
 
 logging.basicConfig(encoding="utf-8", level=logging.INFO, handlers=[handler])
 
