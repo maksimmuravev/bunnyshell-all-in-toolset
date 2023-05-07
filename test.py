fb9fb46a0140ad3a13f1ad9fa8dc716ea32a887a
Maksim Muravev <halloumee@proton.me>
2023-05-07 09:01:37 +0300

    Add decode_percent

--- test.py
+++ test.py
@@ -3,11 +3,17 @@ import logging
 import logging.handlers
 import random
 
-vector_host = 'vector-fhbbb5.bunnyenv.com'
+from urllib.parse import unquote_plus
+
+vector_host = 'vector-xl4avw.bunnyenv.com'
 vector_url = '/'
 handler = logging.handlers.HTTPHandler(vector_host, vector_url, method='POST', secure=True)
 
-logging.basicConfig(level=logging.INFO, handlers=[handler])
+# Set the Content-Type header to text/plain
+handler.setFormatter(logging.Formatter('%(message)s'))
+handler.addFilter(lambda record: not record.args)
+
+logging.basicConfig(encoding="utf-8", level=logging.INFO, handlers=[handler])
 
 while True:
     logging.info(f'This is a random log message: {random.randint(1, 100)}')
