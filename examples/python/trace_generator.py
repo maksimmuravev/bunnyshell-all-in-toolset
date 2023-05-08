9ba2dc57a0e31856cbaf9e603f15f7c96463ac81
Maksim Muravev <halloumee@proton.me>
2023-05-08 10:58:32 +0300

    Otel grpc to http

---
+++ examples/python/trace_generator.py
@@ -0,0 +1,23 @@
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# Resource can be required for some backends, e.g. Jaeger
+# If resource wouldn't be set - traces wouldn't appears in Jaeger
+resource = Resource(attributes={
+    "service.name": "service"
+})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(endpoint="https://opentelemetry-2xp5si.bunnyenv.com")
+
+span_processor = BatchSpanProcessor(otlp_exporter)
+
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+with tracer.start_as_current_span("foo"):
+    print("Hello world!")
