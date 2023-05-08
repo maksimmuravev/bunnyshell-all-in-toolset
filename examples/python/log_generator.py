fcb3a3a721b1d99ebdebb686779f90e2d14ece08
Maksim Muravev <halloumee@proton.me>
2023-05-08 09:03:42 +0300

    Small refactoring; add tracing tools

--- examples/python/log_generator.py
+++ examples/python/log_generator.py
@@ -3,7 +3,7 @@ import logging
 import logging.handlers
 import random
 
-vector_host = 'vector-frpcf6.bunnyenv.com'
+vector_host = 'vector-a6n7sa.bunnyenv.com'
 vector_url = '/'
 handler = logging.handlers.HTTPHandler(vector_host, vector_url, method='POST', secure=True)
 
