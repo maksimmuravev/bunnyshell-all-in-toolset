2aba5e448fd8613d97f4c62f7d652bc6a50624c9
Maksim Muravev <halloumee@proton.me>
2023-05-20 15:06:29 +0300

    Update Sentry Postgres image and lock upgrade

--- components/sentry/.docker/files/entrypoint.sh
+++ components/sentry/.docker/files/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-supervisorctl stop all
-sentry upgrade --noinput
-supervisorctl restart all
-sentry createuser --email admin@admin.local --password admin --superuser
-supervisord -n
+# supervisorctl stop all
+sentry upgrade --noinput --lock
+# supervisorctl restart all
+# sentry createuser --email admin@admin.local --password admin --superuser
+# supervisord -n
