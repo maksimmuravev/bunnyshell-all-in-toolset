fde3cdb63bd26dbed10dde16810a2136874a6430
Maksim Muravev <halloumee@proton.me>
2023-06-05 15:44:27 +0300

    Add prometheus component and pushgateway jobs

---
+++ examples/python/prometheus.py
@@ -0,0 +1,13 @@
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
+# Create a registry to hold the metrics
+registry = CollectorRegistry()
+
+# Create a Gauge metric
+gauge_metric = Gauge("hello_world_metric", "Example metric for Hello World", registry=registry)
+
+# Set the value of the metric
+gauge_metric.set(42)
+
+# Push the metrics to the Prometheus Pushgateway
+push_to_gateway("https://prometheus-6aeh5g.bunnyenv.com/metrics", job="pushgateway", registry=registry)
