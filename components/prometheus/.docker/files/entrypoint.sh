390d686a865f722f8f2f3c0e8bf23992185416ec
Maksim Muravev <halloumee@proton.me>
2023-06-05 18:08:47 +0300

    Add entrypoint script to Dockerfile

---
+++ components/prometheus/.docker/files/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget -O envsubst https://github.com/a8m/envsubst/releases/download/v1.4.2/envsubst-Linux-x86_64
+chmod +x envsubst
+./envsubst < raw_config.yaml > /etc/prometheus/prometheus.yml
+
+/bin/prometheus
