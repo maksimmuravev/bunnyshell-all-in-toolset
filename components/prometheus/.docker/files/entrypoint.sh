2fbc45b8b1a971a2f60b9b9c5e957c18c0301f54
Maksim Muravev <halloumee@proton.me>
2023-06-05 17:28:06 +0300

    Refactor Prometheus Dockerfile and entrypoint script

--- components/prometheus/.docker/files/entrypoint.sh
+++ components/prometheus/.docker/files/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 wget -O envsubst https://github.com/a8m/envsubst/releases/download/v1.4.2/envsubst-Linux-x86_64
 chmod +x envsubst
-./envsubst < raw_config.yaml > /etc/prometheus/prometheus.yml
+./envsubst < raw_config.yaml > /prometheus/prometheus.yml
 
 /bin/prometheus
