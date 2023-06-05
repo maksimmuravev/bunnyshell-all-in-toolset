0ba4cdece48212830e5fe55ab6c0d97fc2751ad5
Maksim Muravev <halloumee@proton.me>
2023-06-05 18:38:18 +0300

    Refactor prometheus docker configuration

--- components/prometheus/.docker/files/entrypoint.sh
+++ components/prometheus/.docker/files/entrypoint.sh
@@ -2,6 +2,6 @@
 
 wget -O envsubst https://github.com/a8m/envsubst/releases/download/v1.4.2/envsubst-Linux-x86_64
 chmod +x envsubst
-./envsubst < raw_config.yaml > /etc/prometheus/prometheus.yml
+./envsubst < raw_config.yaml > /prometheus/prometheus.yml
 
 /bin/prometheus
