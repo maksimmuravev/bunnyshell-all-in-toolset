14c14bf93ed44cb207fa5e384c2399ff379aacc8
Maksim Muravev <halloumee@proton.me>
2023-06-11 15:59:31 +0300

    Update entrypoint script for prometheus configuration

--- components/prometheus/.docker/files/entrypoint.sh
+++ components/prometheus/.docker/files/entrypoint.sh
@@ -3,6 +3,8 @@
 wget -O envsubst https://github.com/a8m/envsubst/releases/download/v1.4.2/envsubst-Linux-x86_64
 chmod +x envsubst
 
+
+export PUSHGATEWAY_URL=${PUSHGATEWAY_URL%?}
 ./envsubst < raw_config.yaml > /prometheus/prometheus.yml
 
 # while true; do
