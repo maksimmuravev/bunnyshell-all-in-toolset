460f92e027d587de60a15f41e839aa2334226171
Maksim Muravev <halloumee@proton.me>
2023-06-11 15:48:38 +0300

    Fix Prometheus datasource type and entrypoint script in Docker

--- components/prometheus/.docker/files/entrypoint.sh
+++ components/prometheus/.docker/files/entrypoint.sh
@@ -3,13 +3,11 @@
 wget -O envsubst https://github.com/a8m/envsubst/releases/download/v1.4.2/envsubst-Linux-x86_64
 chmod +x envsubst
 
-# export PUSHGATEWAY_URL="google.com"
-
 ./envsubst < raw_config.yaml > /prometheus/prometheus.yml
 
-while true; do
-  sleep 3
-  cat /prometheus/prometheus.yml
-done
+# while true; do
+#   sleep 3
+#   cat /prometheus/prometheus.yml
+# done
 
-# /bin/prometheus
+/bin/prometheus
