de19da98900a34a64fd8ffe1136acab2a96a6372
Maxim Muravyev <m_muravyev@wargaming.net>
2023-06-11 15:07:08 +0300

    Add grafana service to prometheus stack

--- examples/python/prometheus.py
+++ examples/python/prometheus.py
@@ -1,13 +1,6 @@
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 
-# Create a registry to hold the metrics
 registry = CollectorRegistry()
-
-# Create a Gauge metric
-gauge_metric = Gauge("hello_world_metric", "Example metric for Hello World", registry=registry)
-
-# Set the value of the metric
-gauge_metric.set(42)
-
-# Push the metrics to the Prometheus Pushgateway
-push_to_gateway("https://prometheus-cshdie.bunnyenv.com/metrics", job="pushgateway", registry=registry)
+g = Gauge('job_last_success_unixtime', 'Last time a batch job successfully finished', registry=registry)
+g.set_to_current_time()
+push_to_gateway('http://pushgateway-kg5jvi.bunnyenv.com', job='pushgateway', registry=registry)
