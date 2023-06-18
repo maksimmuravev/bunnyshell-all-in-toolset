8678ce111293ba3081234cfa17fe3692ec93881d
Maxim Muravyev <m_muravyev@wargaming.net>
2023-06-18 11:04:22 +0300

    Add Python Flask user registration app with logging

--- examples/python/log_generator.py
+++ examples/python/log_generator.py
@@ -2,13 +2,34 @@ import time
 import logging
 import logging.handlers
 import random
+from flask import Flask, request
 
-vector_host = 'vector-a6n7sa.bunnyenv.com'
+app = Flask(__name__)
+
+vector_host = 'vector-nvjqqj.bunnyenv.com'
 vector_url = '/'
 handler = logging.handlers.HTTPHandler(vector_host, vector_url, method='POST', secure=True)
 
 logging.basicConfig(encoding="utf-8", level=logging.INFO, handlers=[handler])
 
-while True:
-    logging.info(f'This is a random log message: {random.randint(1, 100)}')
-    time.sleep(1)
+@app.route('/')
+def index():
+    return 'Welcome to the User Registration App!'
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    
+    # Business logic: Simulating user registration
+    # In a real-life application, you would perform database operations or other actions here
+    if username and email:
+        logging.info(f'New user registered! Username: {username}, Email: {email}')
+        return 'User registered successfully!'
+    else:
+        logging.warning('Failed to register user: invalid data')
+        return 'Invalid data. User registration failed.'
+
+if __name__ == '__main__':
+    app.run()
+
