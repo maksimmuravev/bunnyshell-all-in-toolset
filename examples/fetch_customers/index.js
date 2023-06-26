b54755314ae540f6a5b8c90837699233cc0f20d8
Maksim Muravev <halloumee@proton.me>
2023-06-26 15:16:13 +0300

    Add OpenTelemetry tracing to Express app

--- examples/fetch_customers/index.js
+++ examples/fetch_customers/index.js
@@ -1,40 +1,49 @@
-const express = require('express');
-const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { BasicTracerProvider, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+const express = require('express'); // Express framework
+const { Resource } = require('@opentelemetry/resources'); // OpenTelemetry Resources
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions'); // OpenTelemetry Semantic Conventions
+const { BasicTracerProvider, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base'); // OpenTelemetry Tracer Provider
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http'); // OpenTelemetry OTLP HTTP Exporter
+const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express'); // OpenTelemetry Express Instrumentation
 
-const provider = new NodeTracerProvider({
-  resource: {
-    serviceName: 'example-server'
-  }
+// Create a basic tracer provider
+const provider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'basic-service', // Set the service name for tracing
+  }),
 });
 
 // Configure the OTLP HTTP exporter
 const exporter = new OTLPTraceExporter({
-  url: 'https://opentelemetry-afdml3.bunnyenv.com/v1/traces',
+  url: 'https://opentelemetry-g3mbdc.bunnyenv.com/v1/traces', // Specify the endpoint URL for trace export
 });
 
+// Add a span processor and register the tracer provider
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 provider.register();
 
 // Initialize the Express instrumentation
-const expressInstrumentation = new ExpressInstrumentation();
-expressInstrumentation.setTracerProvider(provider);
+const expressInstrumentation = new ExpressInstrumentation(); // Initialize the Express instrumentation
+expressInstrumentation.setTracerProvider(provider); // Set the tracer provider for Express instrumentation
 
 // Create an Express application
-const app = express();
+const app = express(); // Create an Express application
 
 // Define a route that performs business logic
 app.get('/api/customers', (req, res) => {
-  const tracer = provider.getTracer('example-app');
-  const span = tracer.startSpan('getCustomers');
+  const tracer = provider.getTracer('example-app'); // Get the tracer for the example app
+  const span = tracer.startSpan('getCustomers', {
+    attributes: {
+      'http.method': req.method, // Capture the actual HTTP method used
+      'http.route': req.path, // Capture the route requested
+      'app.custom.attribute': 'some value', // Add a custom attribute specific to your application
+    },
+  }); // Start a new span with the name 'getCustomers'
 
-  // Perform actual business logic here
-  const customers = fetchCustomersFromDatabase();
-  res.json(customers);
+  // Perform actual business logic here (fetch customers from the database)
+  const customers = fetchCustomersFromDatabase(); // Fetch customers from the database
+  res.json(customers); // Send the customers as JSON response
 
-  span.end();
+  span.end(); // End the span
 });
 
 // Simulated function to fetch customers from a database
@@ -47,8 +56,7 @@ function fetchCustomersFromDatabase() {
 }
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Set the port for the server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`); // Log the server's port when it starts
 });
-
